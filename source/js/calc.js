6717dd0b834ed8bceb0bf2ba9d5f5e73a0a87270
Markov Sergey <markov@qtech.ru>
2020-03-20 19:17:27 +0300

    добавляет js для calc

--- source/js/calc.js
+++ source/js/calc.js
@@ -1,7 +1,55 @@
-'use strict';
+"use strict";
 
-const calcValueDef = document.querySelector('.calc-valueDefault');
-console.dir(calcValueDef)
+const inputRealty = document.getElementById("calc-realty");
+const calcPlus = document.getElementById("calc-plus");
+const calcMinus = document.getElementById("calc-minus");
+const calcFirstpay = document.getElementById("calc-firstpay");
+
+
 const valueMin = 1200000;
-const valueMax = 25000000;cs
+const valueMax = 25000000;
+const defValue = 2000000;
+const step = 100000;
+// var calcFirstpayValue = inputRealty.value * 10 / 100;
+
+
+/* Меняет значение при фокусе */
+inputRealty.onfocus = () => {
+  if (inputRealty.value === "") {
+    inputRealty.value = defValue;
+    calcFirstpay.value = inputRealty.value * 10 / 100
+  }
+};
+
+/* Проверка на min max */
+let checkValue = () => {
+  console.log(inputRealty.value)
+  if (inputRealty.value < valueMin) {
+    inputRealty.value = valueMin;
+    calcFirstpay.value = inputRealty.value * 10 / 100
+  }
+  if (inputRealty.value > valueMax) {
+    inputRealty.value = valueMax;
+    calcFirstpay.value = inputRealty.value * 10 / 100
+  }
+}
+
+/* Отслеживание изменения значения */
+inputRealty.onchange = () => {
+  checkValue();
+};
+
+/* Отслеживание клика plus */
+calcPlus.onclick = () => {
+  inputRealty.valueAsNumber += step;
+  checkValue();
+  calcFirstpay.value = inputRealty.value * 10 / 100
+}
+
+/* Отслеживание клика minus */
+calcMinus.onclick = () => {
+  inputRealty.valueAsNumber -= step;
+  checkValue();
+  calcFirstpay.value = inputRealty.value * 10 / 100
+}
 
