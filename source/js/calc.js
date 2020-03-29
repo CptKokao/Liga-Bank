36a99628d89cd32193597782ec321d2f61945940
CptKokaoHome <pro-markov@yamdex.ru>
2020-03-29 20:17:01 +0300

    добавляет параметры для категории

--- source/js/calc.js
+++ source/js/calc.js
@@ -3,16 +3,21 @@
 const inputRealty = document.getElementById("calc-realty");
 const inputFirstpay = document.getElementById("calc-firstpay");
 const inputFirstpayRange = document.getElementById("calc-firstpay-range");
+const inputDateRange = document.getElementById("calc-date-range");
 
-const calcSelect = document.getElementById("calc__select");
+const calcSelect = document.getElementById("calc-select");
+const calcTitle = document.getElementById("calc-title");
+const calcCost = document.getElementById("calc-cost");
+const calcDateFirst = document.getElementById("calc-date-first");
+const calcDateLast = document.getElementById("calc-date-last");
+const calcFirstpayWrap = document.getElementById("calc-firstpay-wrap");
+
 const calcPlus = document.getElementById("calc-plus");
 const calcMinus = document.getElementById("calc-minus");
 const calcProcentValue = document.getElementById("calc-firstpay-value");
 const calcFirstpayError = document.getElementById("calc-fistpay-error");
 
 const calcDate = document.getElementById("calc-date");
-const calcDateRange = document.getElementById("calc-date-range");
-const calcDateValue = document.getElementById("calc-date-value");
 const calcСapital = document.getElementById("calc-capital");
 
 const valueMin = 1200000;
@@ -20,43 +25,118 @@ const valueMax = 25000000;
 const defValue = 2000000;
 const step = 100000;
 const capital = 470000;
-// let minRange;
-// let maxRange;
-let generalSum;
-let procentSum;
-let procRate;
-let procRateMonth;
-let payMonth;
-let sumCredit = 0;
+
+let generalSum; // стоимость недвижимости
+let procentSum; // сумма первоначальный взнос
+let procRate; // % первоначального взноса
+let procRateMonth; // Процентная ставка
+let sumDate = 5; // срок кредитования
+let sumCredit = 0; // сумма кредита(с вычитом первоначального взноса)
+let payMonth; // ежемесячный платеж
+
+/* Получает дефолтное % значение в зависимости от категории */
+const getDefProcent = () => {
+  const hypothecTitle = `Стоимость недвижимость`
+  const autoTitle = `Стоимость автомобиля`
+  const creditTitle = `Сумма потребительского кредита`
+  
+  const hypothecCost = `От 1 200 000  до 25 000 000 рублей`
+  const autoCost = `От 500 000  до 5 000 000 рублей`
+  const creditCost = `От 50 000  до 3 000 000 рублей`
+
+  const hypothecProc = 10;
+  const autoProc = 20;
+
+  const hypothecYearFirst = 5;
+  const hypothecYearLast = 30;
+
+  const autoYearFirst = 1;
+  const autoYearLast = 5;
+
+  const creditYearFirst = 1;
+  const creditYearLast = 7;
+
+  if (calcSelect.value === 'Ипотечное кредитование') {
+    
+    if (calcFirstpayWrap.className === "visually-hidden") {
+      calcFirstpayWrap.classList.remove('visually-hidden');
+    }
+
+    /* добавляет параметры для input */
+    inputFirstpayRange.min = hypothecProc;
+    inputFirstpayRange.max = 100;
+    inputFirstpayRange.step = 5;
+    inputFirstpayRange.value = hypothecProc;
+
+    inputDateRange.value = hypothecYearFirst;
+    inputDateRange.min = hypothecYearFirst;
+    inputDateRange.max = 30;
+    inputDateRange.step = 1;
+
+    /* выводим % в html */
+    calcTitle.textContent = hypothecTitle;
+    calcCost.textContent = hypothecCost;
+    calcProcentValue.textContent = hypothecProc;
+    calcDateFirst.textContent = hypothecYearFirst + ' лет';
+    calcDateLast.textContent = hypothecYearLast + ' лет';
+
+
+  } else 
+  if (calcSelect.value === 'Автомобильное кредитование') {
+
+    if (calcFirstpayWrap.className === "visually-hidden") {
+      calcFirstpayWrap.classList.remove('visually-hidden');
+    }
+
+    /* добавляет value для input */
+    inputFirstpayRange.value = autoProc;
+    inputFirstpayRange.min = autoProc;
+    inputFirstpayRange.max = 100;
+    inputFirstpayRange.step = 5;
+
+    inputDateRange.min = autoYearFirst;
+    inputDateRange.max = 5;
+    inputDateRange.step = 1;
+    inputDateRange.value = autoYearFirst;
+
+    /* выводим % в html */
+    calcTitle.textContent = autoTitle;
+    calcCost.textContent = autoCost;
+    calcProcentValue.textContent = autoProc;
+    calcDateFirst.textContent = autoYearFirst + ' лет';
+    calcDateLast.textContent = autoYearLast + ' лет';
+  } else 
+  if (calcSelect.value === 'Потребительский кредит') {
+    console.dir(calcFirstpayWrap)
+    /* добавляет value для input */
+    inputDateRange.min = creditYearFirst;
+    inputDateRange.max = 7;
+    inputDateRange.step = 1;
+    inputDateRange.value = creditYearFirst;
+
+    /* выводим % в html */
+    calcTitle.textContent = creditTitle;
+    calcCost.textContent = creditCost;
+    calcDateFirst.textContent = creditYearFirst + ' лет';
+    calcDateLast.textContent = creditYearLast + ' лет';
+
+    /* Удаляет блок */
+    calcFirstpayWrap.classList.add('visually-hidden');
+  }
+};
 
 /* получает ежемесячный платеж */
 const formula = () => {
-  // payMonth = sumCredit * (procRateMonth + procRateMonth/((1+procRateMonth)*12)-1);
-  // payMonth = (sumCredit * procRateMonth) - (1 - 1/(1 + procRateMonth)* 12)
+  let countPeriods = sumDate * 12;
+  payMonth = (sumCredit * procRateMonth) - (1 - 1/Math.pow((1 + procRateMonth),countPeriods))
 } 
 
-// const getMinMax = () => {
-//   minRange = generalSum / 100 * 10
-//   maxRange = generalSum;
-// } 
-
-/* Проверка суммы первоначального взноса */
-// const chcekSum = (target, error) => {
-//   /* Если валидность false */
-//   if (!target.checkValidity()) {
-//     /* Если значение больше */
-//     if (target.validity.rangeUnderflow) {
-//       error.textContent = `Взнос должен быть больше ${minRange}`
-//     } else
-//     /* Если значение меньше */
-//     if (target.validity.rangeOverflow) {
-//       error.textContent = `Взнос должен быть меньше ${maxRange}`
-//     }
-//   /* Если валидность true */
-//   } else {
-//     error.textContent = ``;
-//   }
-// }
+/* Отслеживание изменения срока кредитования */
+inputDateRange.oninput = (e) => {
+  let date = e.target.value;
+  calcDateValue.textContent = date;
+  calcDate.value = `${date} лет`;
+};
 
 /* Проверка на min max */
 let checkValue = () => {
@@ -85,27 +165,13 @@ let checkValue = () => {
   }
 };
 
-/* Получает дефолтное % значение в зависимости от категории */
-const getDefProcent = () => {
-  const hypothecProc = 10;
-  const autoProc = 20;
-
-  if (calcSelect.value === 'Ипотечное кредитование') {
-    /* добавляет value для input */
-    inputFirstpayRange.value = hypothecProc;
-    /* выводим % в html */
-    calcProcentValue.textContent = hypothecProc;
-  } else 
-  if (calcSelect.value === 'Автомобильное кредитование') {
-    /* добавляет value для input */
-    inputFirstpayRange.value = autoProc;
-    /* выводим % в html */
-    calcProcentValue.textContent = autoProc;
-  }
-};
-
 /* Получает дефолтные % и выводим в html */
 getDefProcent();
+
+/* Событие при изменении категории */
+calcSelect.onchange = () => {
+  getDefProcent();
+}
 
 /* Получает сумму % от общей суммы */
 const getProcentSum = () => {
@@ -194,9 +260,9 @@ calcСapital.oninput = () => {
 };
 
 /* Отслеживание изменения срока кредитования */
-calcDateRange.oninput = (e) => {
-  let date = e.target.value;
-  calcDateValue.textContent = date;
-  calcDate.value = `${date} лет`;
+inputDateRange.oninput = (e) => {
+  sumDate = e.target.value;
+  calcDateFirst.textContent = sumDate + ' лет';
+  inputDateRange.value = sumDate;
 };
 
