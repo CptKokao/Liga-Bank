40ce8544e59e2877f995a4f98dc49f4ca98527d8
Markov Sergey <markov@qtech.ru>
2020-04-10 18:03:13 +0300

    Исправляет модальное окно

--- build/js/script.js
+++ build/js/script.js
@@ -58,227 +58,6 @@ $("#phone").mask("8(999) 999-9999");
     }
   }
 })();
-'use strict';
-/* Yandex map */
-
-ymaps.ready(init);
-
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map('map', {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [55.58873129, 56.70376289],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 3,
-    controls: ['zoomControl', 'geolocationControl'],
-    behaviors: ['drag']
-  }); // Функция, которая по состоянию чекбоксов в меню
-  // показывает или скрывает геообъекты из выборки.
-
-  function checkState() {
-    var shownObjects;
-    var byCity = new ymaps.GeoQueryResult(); // Отберем объекты по форме.
-
-    if ($('#rus').prop('checked')) {
-      byCity = myObjects.search('options.iconContent = "RUS"');
-    }
-
-    if ($('#cng').prop('checked')) {
-      byCity = myObjects.search('options.iconContent = "CNG"').add(byCity);
-    }
-
-    if ($('#euro').prop('checked')) {
-      byCity = myObjects.search('options.iconContent = "EURO"').add(byCity);
-    } // Мы отобрали объекты по цвету и по форме. Покажем на карте объекты,
-    // которые совмещают нужные признаки.
-
-
-    shownObjects = byCity.addToMap(myMap); // Объекты, которые не попали в выборку, нужно убрать с карты.
-
-    myObjects.remove(shownObjects).removeFromMap(myMap);
-  }
-
-  $('#rus').click(checkState);
-  $('#cng').click(checkState);
-  $('#euro').click(checkState); // Создадим объекты из их JSON-описания и добавим их на карту.
-
-  window.myObjects = ymaps.geoQuery({
-    type: 'FeatureCollection',
-    features: [{
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [55.75897861, 37.6158744]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [58.59281591, 49.66310862]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [51.55538262, 46.059593]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [57.18885551, 65.57131175]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [55.00336661, 73.30568675]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [59.94241846, 30.2392805]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'RUS'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [40.36391621, 49.83888987]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'CNG'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [41.29850313, 69.30666331]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'CNG'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [53.87639833, 27.55861644]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'CNG'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [43.22421706, 76.90920237]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'CNG'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [48.9644317, 2.29006175]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'EURO'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [50.05179937, 14.50685862]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'EURO'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [51.61013852, -0.08298513]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'EURO'
-      }
-    }, {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [42.05625121, 12.6611555]
-      },
-      options: {
-        iconLayout: 'default#image',
-        iconImageHref: '../img/location.svg',
-        iconImageSize: [30, 35],
-        iconContent: 'EURO'
-      }
-    }]
-  }).addToMap(myMap);
-}
 "use strict";
 
 var body = document.querySelector("body");
@@ -879,17 +658,19 @@ var esc = 27;
 var overlay = document.getElementById("overlay");
 var offrOverlay = document.getElementById("offer-overlay");
 var iconClose = document.getElementById("close-overlay");
+var inputLogin = document.getElementById("login");
 
 var closeOverlay = function closeOverlay(target) {
-  overlay.classList.add('visually-hidden');
-  target.classList.add('visually-hidden');
-  body.style.overflow = 'auto';
+  overlay.classList.add("visually-hidden");
+  target.classList.add("visually-hidden");
+  body.style.overflow = "auto";
 };
 
 var openOverlay = function openOverlay(target) {
-  overlay.classList.remove('visually-hidden');
-  target.classList.remove('visually-hidden');
-  body.style.overflow = 'hidden';
+  overlay.classList.remove("visually-hidden");
+  target.classList.remove("visually-hidden");
+  body.style.overflow = "hidden";
+  inputLogin.focus();
 };
 /* закрытие модального окна */
 
@@ -898,10 +679,10 @@ iconClose.onclick = function () {
   closeOverlay(offrOverlay);
 };
 
-window.addEventListener('keydown', function (e) {
+window.addEventListener("keydown", function (e) {
   console.log(e);
 
-  if (e.code === 'Escape' || e.keyCode === esc) {
+  if (e.code === "Escape" || e.keyCode === esc) {
     closeOverlay(offrOverlay);
   }
 });
@@ -922,18 +703,18 @@ modalClose.onclick = function () {
 };
 
 overlay.onclick = function (e) {
-  if (modal.classList.contains('visually-hidden')) {
+  if (modal.classList.contains("visually-hidden")) {
     closeOverlay(offrOverlay);
-  } else if (offrOverlay.classList.contains('visually-hidden')) {
+  } else if (offrOverlay.classList.contains("visually-hidden")) {
     closeOverlay(modal);
   }
 };
 
-window.addEventListener('keydown', function (e) {
-  if (e.code === 'Escape' || e.keyCode === esc) {
-    if (modal.classList.contains('visually-hidden')) {
+window.addEventListener("keydown", function (e) {
+  if (e.code === "Escape" || e.keyCode === esc) {
+    if (modal.classList.contains("visually-hidden")) {
       closeOverlay(offrOverlay);
-    } else if (offrOverlay.classList.contains('visually-hidden')) {
+    } else if (offrOverlay.classList.contains("visually-hidden")) {
       closeOverlay(modal);
     }
   }
@@ -1000,3 +781,224 @@ var getOfferReset = function getOfferReset() {
 var getRequest = function getRequest() {
   requestWrap.insertAdjacentHTML("afterbegin", "<div class=\"calc__request-item\">\n        <p>\u041D\u043E\u043C\u0435\u0440 \u0437\u0430\u044F\u0432\u043A\u0438</p>\n        <span>\u2116 ".concat(requestNumber, "</span>\n        </div>\n  \n        <div class=\"calc__request-item\">\n        <p>\u0426\u0435\u043B\u044C \u043A\u0440\u0435\u0434\u0438\u0442\u0430</p>\n        <span>").concat(requestTarget, "</span>\n        </div>\n  \n        <div class=\"calc__request-item\">\n        <p>\u0421\u0442\u043E\u0438\u043C\u043E\u0441\u0442\u044C \u043D\u0435\u0434\u0432\u0438\u0436\u0438\u043C\u043E\u0441\u0442\u0438</p>\n        <span>").concat(generalSum, " \u0440\u0443\u0431\u043B\u0435\u0439</span>\n        </div>\n  \n        <div class=\"calc__request-item\">\n        <p>\u041F\u0435\u0440\u0432\u043E\u043D\u0430\u0447\u0430\u043B\u044C\u043D\u044B\u0439 \u0432\u0437\u043D\u043E\u0441</p>\n        <span>").concat(procentSum, " \u0440\u0443\u0431\u043B\u0435\u0439</span>\n        </div>\n  \n        <div class=\"calc__request-item\">\n        <p>\u0421\u0440\u043E\u043A \u043A\u0440\u0435\u0434\u0438\u0442\u043E\u0432\u0430\u043D\u0438\u044F</p>\n        <span>").concat(dateSum, " \u043B\u0435\u0442</span>\n      </div>"));
 };
+'use strict';
+/* Yandex map */
+
+ymaps.ready(init);
+
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map('map', {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [55.58873129, 56.70376289],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 3,
+    controls: ['zoomControl', 'geolocationControl'],
+    behaviors: ['drag']
+  }); // Функция, которая по состоянию чекбоксов в меню
+  // показывает или скрывает геообъекты из выборки.
+
+  function checkState() {
+    var shownObjects;
+    var byCity = new ymaps.GeoQueryResult(); // Отберем объекты по форме.
+
+    if ($('#rus').prop('checked')) {
+      byCity = myObjects.search('options.iconContent = "RUS"');
+    }
+
+    if ($('#cng').prop('checked')) {
+      byCity = myObjects.search('options.iconContent = "CNG"').add(byCity);
+    }
+
+    if ($('#euro').prop('checked')) {
+      byCity = myObjects.search('options.iconContent = "EURO"').add(byCity);
+    } // Мы отобрали объекты по цвету и по форме. Покажем на карте объекты,
+    // которые совмещают нужные признаки.
+
+
+    shownObjects = byCity.addToMap(myMap); // Объекты, которые не попали в выборку, нужно убрать с карты.
+
+    myObjects.remove(shownObjects).removeFromMap(myMap);
+  }
+
+  $('#rus').click(checkState);
+  $('#cng').click(checkState);
+  $('#euro').click(checkState); // Создадим объекты из их JSON-описания и добавим их на карту.
+
+  window.myObjects = ymaps.geoQuery({
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [55.75897861, 37.6158744]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [58.59281591, 49.66310862]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [51.55538262, 46.059593]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [57.18885551, 65.57131175]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [55.00336661, 73.30568675]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [59.94241846, 30.2392805]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'RUS'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [40.36391621, 49.83888987]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'CNG'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [41.29850313, 69.30666331]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'CNG'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [53.87639833, 27.55861644]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'CNG'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [43.22421706, 76.90920237]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'CNG'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [48.9644317, 2.29006175]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'EURO'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [50.05179937, 14.50685862]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'EURO'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [51.61013852, -0.08298513]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'EURO'
+      }
+    }, {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [42.05625121, 12.6611555]
+      },
+      options: {
+        iconLayout: 'default#image',
+        iconImageHref: '../img/location.svg',
+        iconImageSize: [30, 35],
+        iconContent: 'EURO'
+      }
+    }]
+  }).addToMap(myMap);
+}
--- source/js/scripts/calc/modal.js
+++ source/js/scripts/calc/modal.js
@@ -3,31 +3,32 @@ var esc = 27;
 const overlay = document.getElementById("overlay");
 const offrOverlay = document.getElementById("offer-overlay");
 const iconClose = document.getElementById("close-overlay");
+const inputLogin = document.getElementById("login");
 
 let closeOverlay = (target) => {
-  overlay.classList.add('visually-hidden');
-  target.classList.add('visually-hidden');
-  body.style.overflow = 'auto';
+  overlay.classList.add("visually-hidden");
+  target.classList.add("visually-hidden");
+  body.style.overflow = "auto";
 };
 
 let openOverlay = (target) => {
-  overlay.classList.remove('visually-hidden');
-  target.classList.remove('visually-hidden');
-  body.style.overflow = 'hidden';
+  overlay.classList.remove("visually-hidden");
+  target.classList.remove("visually-hidden");
+  body.style.overflow = "hidden";
+  inputLogin.focus();
 };
 
 /* закрытие модального окна */
 iconClose.onclick = () => {
   closeOverlay(offrOverlay);
-}
+};
 
-window.addEventListener('keydown', function (e) {
-  console.log(e)
-  if (e.code === 'Escape' || e.keyCode === esc) {
+window.addEventListener("keydown", function (e) {
+  console.log(e);
+  if (e.code === "Escape" || e.keyCode === esc) {
     closeOverlay(offrOverlay);
   }
 });
-
 
 /* Модальное окно */
 const enterCabinet = document.getElementById("enter-cabinet");
@@ -38,27 +39,25 @@ const inputPassword = document.getElementById("password");
 
 enterCabinet.onclick = () => {
   openOverlay(modal);
-}
+};
 
 modalClose.onclick = () => {
   closeOverlay(modal);
-}
+};
 
 overlay.onclick = (e) => {
-  if (modal.classList.contains('visually-hidden')) {
+  if (modal.classList.contains("visually-hidden")) {
     closeOverlay(offrOverlay);
-  } else
-  if (offrOverlay.classList.contains('visually-hidden')) {
+  } else if (offrOverlay.classList.contains("visually-hidden")) {
     closeOverlay(modal);
   }
-}
+};
 
-window.addEventListener('keydown', function (e) {
-  if (e.code === 'Escape' || e.keyCode === esc) {
-    if (modal.classList.contains('visually-hidden')) {
+window.addEventListener("keydown", function (e) {
+  if (e.code === "Escape" || e.keyCode === esc) {
+    if (modal.classList.contains("visually-hidden")) {
       closeOverlay(offrOverlay);
-    } else
-    if (offrOverlay.classList.contains('visually-hidden')) {
+    } else if (offrOverlay.classList.contains("visually-hidden")) {
       closeOverlay(modal);
     }
   }
@@ -67,8 +66,8 @@ window.addEventListener('keydown', function (e) {
 showPassword.onclick = () => {
   console.dir(inputPassword);
   if (inputPassword.type === "password") {
-    inputPassword.type = "text"
+    inputPassword.type = "text";
   } else {
-    inputPassword.type = "password"
+    inputPassword.type = "password";
   }
-}
+};
